1171551575a028a679fb8697dcb7a97605047b82
ricardo <ricardo.etcheto@gmail.com>
2022-07-16 15:45:51 -0300

    asd

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,4 @@ WORKDIR /work
 
 RUN apk update && apk add openjdk8 && apk add apache-ant
 
-RUN echo "asdf"
-RUN ls
-
 ENTRYPOINT [ "ant" ]
--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -9,5 +9,5 @@ echo $PWD
 $PWD
 echo "aca hay"
 ls
-docker build -t ant:1.0 ./integration/ant --no-cache
+docker build -t ant:1.0 ./integration/ant
 docker run --rm -v $PWD:/work ant:1.0
