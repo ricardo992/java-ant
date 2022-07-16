f28ffd57f6538c55540246667b7b560b08f01bf8
ricardo <ricardo.etcheto@gmail.com>
2022-07-16 03:23:12 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -7,10 +7,7 @@ echo "****************"
 echo "estamos en:"
 echo $PWD
 $PWD
-echo "estamos en2:"
-echo $(PWD)
-$(PWD)
 echo "aca hay"
 ls
 docker build -t ant:1.0 ./integration/ant 
-docker run --rm -v /:/work ant:1.0 -f build.xml
+docker run --rm -v $PWD:/work ant:1.0 
