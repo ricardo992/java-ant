6693482f886002084ab162a45b4b241de5e974dc
ricardo <ricardo.etcheto@gmail.com>
2022-07-17 01:05:08 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -7,4 +7,4 @@ echo "****************"
 PROJ=$PWD
 
 docker build -t ant:1.0 ./integration/ant --no-cache
-docker run --rm -v $PROJ:/work ant:1.0 ls -la ./work
+docker run --rm -v /var/jenkins_home/workspace/java-ant:/work ant:1.0 ls -la ./work
