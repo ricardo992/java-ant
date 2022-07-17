f1e2cb798ab75f01d6ef9d78c5c6c694ea6e387b
ricardo <ricardo.etcheto@gmail.com>
2022-07-17 01:10:27 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -7,4 +7,4 @@ echo "****************"
 PROJ=$PWD
 
 docker build -t ant:1.0 ./integration/ant --no-cache
-docker run --rm -v /var/jenkins_home/workspace/java-ant:/work ant:1.0 ls -la ./work
+docker run --rm -v //wsl$/Ubuntu-20.04/home/ricardo/proyectos/aplicacionesWeb/prueba-jenkins-docker-in-docker/integration/jenkins_home/workspace/java-ant:/work ant:1.0 ls -la ./work
