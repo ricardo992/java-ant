e44609d40b2f4e72eb1b0d42f85161d0da825209
ricardo <ricardo.etcheto@gmail.com>
2022-07-17 00:52:16 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -5,4 +5,4 @@ echo "* Building jar!*"
 echo "****************"
 
 docker build -t ant:1.0 ./integration/ant --no-cache
-docker run --rm -v ${PWD}:/work ant:1.0 ls ./work
+docker run --rm -v /$(pwd):/work ant:1.0 ls ./work
