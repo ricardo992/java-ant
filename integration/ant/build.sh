b9a790ea918ea7411c9912e2cb34c9764efadabe
ricardo <ricardo.etcheto@gmail.com>
2022-07-19 20:56:07 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -4,7 +4,7 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
-# docker build -t ant:2.0 ./integration/ant --no-cache
-# docker run --rm -v $PWD:/work ant:2.0 ls
+docker build -t ant:2.0 ./integration/ant --no-cache
+docker run --rm -v $PWD:/work ant:2.0 ls
 
-cd integration/ant && docker-compose -f docker-compose-build.yml build --no-cache
+# cd integration/ant && docker-compose -f docker-compose-build.yml build --no-cache
