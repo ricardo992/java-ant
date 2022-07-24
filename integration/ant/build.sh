2c553a10bf890934e42dfe94f0a4bb5307fd4bfe
ricardo <ricardo.etcheto@gmail.com>
2022-07-24 18:11:09 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -5,7 +5,7 @@ echo "* Building jar!*"
 echo "****************"
 
 docker build -t ant:2.0 ./integration/ant --no-cache
-# docker run --rm -v $PWD:/work ant:2.0 ls
-docker run --rm ant:2.0 ls
+docker run --rm -v //wsl$/Ubuntu-20.04/home/ricardo/proyectos/aplicacionesWeb/prueba-jenkins-docker-in-docker/integration/jenkins_home/workspace/java-ant:/work ant:2.0 ls
+# docker run --rm ant:2.0 ls
 
 # cd integration/ant && docker-compose -f docker-compose-build.yml build --no-cache
