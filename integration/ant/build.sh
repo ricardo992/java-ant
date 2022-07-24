7cc6ae9d46f211b30788805c2ac5fc710aa1ecbd
ricardo <ricardo.etcheto@gmail.com>
2022-07-24 19:51:15 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -4,11 +4,5 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
-echo "estamos en:"
-$PWD
-echo "arriba"
-
-PROJ=$PWD
-
 docker build -t ant:2.0 ./integration/ant --no-cache
 docker run --rm -v $PROJ:/worka ant:2.0
