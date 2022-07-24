dcfdcedca6c20f76e2020e95d5658e498ff033a6
ricardo <ricardo.etcheto@gmail.com>
2022-07-24 19:42:19 -0300

    asd

--- integration/ant/build.sh
+++ integration/ant/build.sh
@@ -7,7 +7,7 @@ echo "****************"
 PROJ=/home/ricardo/proyectos/aplicacionesWeb/prueba-jenkins-docker-in-docker/integration/jenkins_home/workspace/java-ant
 
 docker build -t ant:2.0 ./integration/ant --no-cache
-docker run --rm -v $PROJ:/worka ant:2.0 ls
+docker run --rm -v $PROJ:/worka ant:2.0
 # docker run --rm ant:2.0 ls
 
 # cd integration/ant && docker-compose -f docker-compose-build.yml build --no-cache
